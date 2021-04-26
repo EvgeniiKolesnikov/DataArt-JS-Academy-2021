81e7c6e7d526d0a1d0afdd2c295e7a19553cec2f
EvgeniiKolesnikov <jekow@yandex.ru>
2021-04-26 18:42:53 +0300

    add first design read list

--- ControlWork JS/toReadList/src/js/read-list.js
+++ ControlWork JS/toReadList/src/js/read-list.js
@@ -1,1 +1,18 @@
 'use strict';
+
+import { Storage } from "./storage";
+
+export class ReadList {
+  books = [];
+  storage = new Storage();
+
+  constructor() {
+    console.log('ReadList');
+
+    this.readListBookCount = document.getElementById("readListBookCount");
+    this.readListReadCount = document.getElementById("readListReadCount");
+    this.readList = document.getElementById("readList");
+    this.readListBook = document.getElementById("readListBook");
+    this.readListRead = document.getElementById("readListRead");
+  }
+}
