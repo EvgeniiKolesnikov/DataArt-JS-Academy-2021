635e1fb53a17028645fc061047f6fe9d8449e7b8
EvgeniiKolesnikov <jekow@yandex.ru>
2021-04-27 09:48:58 +0300

    Work menu buttons for max-width 500

--- ControlWork JS/toReadList/src/js/menu-controller.js
+++ ControlWork JS/toReadList/src/js/menu-controller.js
@@ -11,27 +11,35 @@ export class MenuController {
     this.centerBlock =   document.querySelector("#centerBlock");
     this.rightBlock =   document.querySelector("#rightBlock");
     
+    window.addEventListener(`resize`, e => {
+      console.log(e.target.innerHeight);
+      console.log(e.target.innerWidth);
+      // this.leftBlock.style.display = '';
+      // this.centerBlock.style.display = '';
+      // this.rightBlock.style.display = '';
+    }, false);
 
-    onReadListBtn.addEventListener('click', e => {
-      console.log('onReadListBtn');
-      if (this.rightBlock.style.display == 'none') {
-        this.rightBlock.style.display = 'block';
-      } else {
-        this.rightBlock.style.display = 'none'
-      }
-      
-    });
+
 
     onInfoListBtn.addEventListener('click', e => {
       console.log('onInfoListBtn');
-      if (this.centerBlock.style.display == 'none') {
-        this.centerBlock.style.display = 'block';
-      } else {
-        this.centerBlock.style.display = 'none'
-      }
-      
+
+      this.centerBlock.style.zIndex = 0;
+      this.rightBlock.style.zIndex = -10;
 
     });
+
+    onReadListBtn.addEventListener('click', e => {
+      console.log('onReadListBtn');
+      this.centerBlock.style.zIndex = -10;
+      this.rightBlock.style.zIndex = 0;
+
+      // if (this.rightBlock.style.display == 'none' || this.rightBlock.style.display == '') {
+      //   this.rightBlock.style.display = 'block';
+      //   // this.centerBlock.style.display = 'none';
+      // } 
+    });
+
   }
 }
 
